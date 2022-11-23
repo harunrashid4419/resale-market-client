bf683403085d9b99358fb525f5c411f68b4d3881
harunorrashid4419 <harunrashid4419@gmail.com>
2022-11-24 00:09:20 +0600

    login form add

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgGGa_tEOSzq0u_J-6lqTFh1HtCUPnS6w",
+  authDomain: "resale-market-df27a.firebaseapp.com",
+  projectId: "resale-market-df27a",
+  storageBucket: "resale-market-df27a.appspot.com",
+  messagingSenderId: "1056640074842",
+  appId: "1:1056640074842:web:9d1bff5608bc6e090a8463"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+import { FaGoogle } from "react-icons/fa";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+   return (
+      <div className="form">
+         <h1 className="text-white text-4xl mb-8 font-bold text-center">
+            Login
+         </h1>
+         <form>
+            <div className="form-control">
+               <label className="label">
+                  <span className="label-text text-white">Email</span>
+               </label>
+               <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+               />
+            </div>
+            <div className="form-control">
+               <label className="label">
+                  <span className="label-text text-white">Password</span>
+               </label>
+               <input
+                  type="password"
+                  placeholder="*******"
+                  className="input input-bordered"
+               />
+            </div>
+            <label id="forget" className="label">
+               <Link className="label-text text-white mt-3">
+                  <p>Forget Password?</p>
+               </Link>
+            </label>
+            <div className="form-control mt-3">
+               <button className="btn btn-primary">Login</button>
+            </div>
+         </form>
+         <div className="divider text-white">OR</div>
+         <Link>
+            <div className="bg-white p-3 rounded-md">
+               <p className="fond-bold text-xl flex items-center justify-center">
+                  <FaGoogle className="mr-2" />
+                  Sign in with google
+               </p>
+            </div>
+         </Link>
+         <p className="text-white text-center mt-6 text-xl">
+            Don't have any account{" "}
+            <Link className="text-primary" to="/register">
+               Register
+            </Link>{" "}
+         </p>
+      </div>
+   );
 };
 
 export default Login;
