6ea8dd98cd678b02d94b4ed588472a8180382f07
harunorrashid4419 <harunrashid4419@gmail.com>
2022-11-25 01:52:34 +0600

    extra section add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AllCategory from './Categories/AllCategory';
 import Slider from './Slider/Slider';
+import SoldSection from '../Others/SoldSection/SoldSection';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
             <AllCategory></AllCategory>
+            <SoldSection></SoldSection>
         </div>
     );
 };
