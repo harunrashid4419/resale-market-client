b0bc5411fc48e301cdad04a1a2c101854efb70f1
harunorrashid4419 <harunrashid4419@gmail.com>
2022-11-25 23:10:06 +0600

    seller add and delete handle

--- src/Pages/Dashboard/AllSeller/AllSeller.js
+++ src/Pages/Dashboard/AllSeller/AllSeller.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import Seller from "./Seller";
 
 const AllSeller = () => {
-    return (
-        <div>
-            <p>all seller</p>
-        </div>
-    );
+   const { data: users = [], refetch } = useQuery({
+      queryKey: ["users"],
+      queryFn: async () => {
+         const res = await fetch("http://localhost:5000/users");
+         const data = await res.json();
+         return data;
+      },
+   });
+
+   return (
+      <div>
+         <p className="text-3xl text-primary py-5">All Seller</p>
+         <div className="overflow-x-auto">
+            <table className="table w-full">
+               <thead>
+                  <tr>
+                     <th>Image</th>
+                     <th>Name</th>
+                     <th>Email</th>
+                     <th>Action</th>
+                  </tr>
+               </thead>
+               {users.map((user, i) => (
+                  <Seller key={user._id} i={i} refetch={refetch} user={user}></Seller>
+               ))}
+            </table>
+         </div>
+      </div>
+   );
 };
 
 export default AllSeller;
---
+++ src/Pages/Dashboard/AllSeller/Seller.js
@@ -0,0 +1,43 @@
+import React from "react";
+import toast from "react-hot-toast";
+
+const Seller = ({ user, refetch }) => {
+    const {photoURL, name, email, _id} = user;
+
+    const handleDelete = id =>{
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.deletedCount){
+                    toast.success('delete successfully');
+                    refetch();
+                }
+            })
+    }
+
+   return (
+        <tbody>
+         {user.role === "Seller" && (
+            <tr>
+               <th>
+                  <div className="avatar">
+                     <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img src={photoURL} alt='' />
+                     </div>
+                  </div>
+               </th>
+               <td>{name}</td>
+               <td>{email}</td>
+               <td>
+                  <button onClick={() => handleDelete(_id)} className="btn btn-sm">Delete</button>
+               </td>
+            </tr>
+         )}
+      </tbody>
+   );
+};
+
+export default Seller;
